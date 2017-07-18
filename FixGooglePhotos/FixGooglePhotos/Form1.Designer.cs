4033c0396eba516d91f5ef443adad086d2e70a74
Sharon Rimer <crs2007@gmail.com>
2017-07-18 10:37:15 +0300

    First upload

---
+++ FixGooglePhotos/FixGooglePhotos/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FixGooglePhotos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.directoryPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(387, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fix";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // directoryPath
+            // 
+            this.directoryPath.Location = new System.Drawing.Point(12, 89);
+            this.directoryPath.Name = "directoryPath";
+            this.directoryPath.Size = new System.Drawing.Size(387, 20);
+            this.directoryPath.TabIndex = 1;
+            this.directoryPath.Text = "D:\\GoogleDrive";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 198);
+            this.Controls.Add(this.directoryPath);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Google Photos Claen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox directoryPath;
+    }
+}
+
---
+++ FixGooglePhotos/FixGooglePhotos/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixGooglePhotos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(directoryPath.Text))
+            {
+                MessageBox.Show("Please make sure directory path have value", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            string vdirectoryPath;
+            string vFileType = "json";
+            vdirectoryPath = directoryPath.Text;
+            if(!directoryPath.Text.EndsWith(@"\"))
+                vdirectoryPath += @"\";
+
+            DialogResult res = MessageBox.Show(String.Concat("Delete All ", vFileType.ToUpper(), " Files?"), "Delete Verification", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                deleteAllFilesByType(vdirectoryPath, vFileType);
+                moveAllFilesByType(vdirectoryPath, vFileType);
+                deleteAllEmptyDirectory(vdirectoryPath);
+            }
+        }
+
+        private void deleteAllFilesByType(string directoryPath,string extension)
+        {
+            if (!Directory.Exists(directoryPath))
+                return;
+
+            var fileGenerationDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), directoryPath));
+            fileGenerationDir.GetFiles(String.Concat("*.", extension), SearchOption.AllDirectories).ToList().ForEach(file => file.Delete());
+           
+        }
+
+        private void moveAllFilesByType(string directoryPath, string extension)
+        {
+            if (!Directory.Exists(directoryPath))
+                return;
+
+            var fileGenerationDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), directoryPath));
+            fileGenerationDir.GetFiles(String.Concat("*.", extension), SearchOption.AllDirectories).ToList().ForEach(file => file.MoveTo(directoryPath));
+
+        }
+
+
+        private void deleteAllEmptyDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+                return;
+
+            foreach (var directory in Directory.GetDirectories(directoryPath))
+            {
+                deleteAllEmptyDirectory(directoryPath);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+    }
+}
